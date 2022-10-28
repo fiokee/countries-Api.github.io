29a2e9a78249d62fa1f9ac0a605c3b22d02a4d80
fiokee <smleffiong@gmail.com>
2022-10-28 21:32:24 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+import {BsMoon} from 'react-icons/bs';
+
+export const Header = () => {
+  return (
+    <div className='nav-header'>
+        <h2>Where in the World</h2>
+        <h2 className='dark'><BsMoon/>Dark Mode</h2>
+    </div>
+  )
+}
